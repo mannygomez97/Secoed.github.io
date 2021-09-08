9f7055f5597d15f41477d95a3e82adff55038d73
Criss Intriago <jintriagos_96@outlook.com>
2021-09-08 12:01:06 -0500

    Subida del docker

--- secoed/settings.py
+++ secoed/settings.py
@@ -101,18 +101,21 @@ WSGI_APPLICATION = 'secoed.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': 'db_secoed',
-        #'USER': 'postgres',
-        #'PASSWORD': 'postgres',
-        #'HOST': 'pgdb',
-        #'PORT': 5432,
+        #DATABASE PRODUCTION
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db_secoed',
-        'USER': 'secoed',
-        'PASSWORD': 'secoed2021',
-        'HOST': '95.216.216.98',
-        'PORT': 5433,
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'pgdb',
+        'PORT': 5432,
+
+        #DATABASE DEVELOPER
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'db_secoed',
+        #'USER': 'secoed',
+        #'PASSWORD': 'secoed2021',
+        #'HOST': '95.216.216.98',
+        #'PORT': 5433,
     }
 }
 
