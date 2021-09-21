e9e00ad6bbcd6a50d81decedca99130196425f3e
Criss Intriago <jintriagos_96@outlook.com>
2021-09-21 14:54:40 -0500

    ok

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from cursos import views
 
 urlpatterns = [
-    path(r'categoria', login_required(views.CursoView.as_view()), name='categoria'),
+    path(r'categoria', login_required(views.CursoView.categoria), name='categoria'),
     path(r'allCategorias', login_required(views.CursoView.allCategorias), name='categoriaAll'),
     path(r'deleteCategoria/<int:idCategoria>', login_required(views.CursoView.deleteCategoria), name='deleteCategoria'),
     path(r'createEditCategoria', login_required(views.CursoView.createEditCategoria), name='createEditCategoria'),
