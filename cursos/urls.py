8daee622de708cdf8af3552a1bb61dcf67dece60
Criss Intriago <jintriagos_96@outlook.com>
2021-09-21 14:52:51 -0500

    cursos

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from cursos import views
 
 urlpatterns = [
-    path(r'categoria', views.CursoView.categoria, name='categoria'),
-    path(r'allCategorias', views.CursoView.allCategorias, name='categoriaAll'),
-    path(r'deleteCategoria/<int:idCategoria>', views.CursoView.deleteCategoria, name='deleteCategoria'),
-    path(r'createEditCategoria', views.CursoView.createEditCategoria, name='createEditCategoria'),
-    path(r'', views.CursoView.as_view(), name='cursos'),
-    path(r'crearEditarCurso', views.CursoView.crearEditarCurso, name='crearEditarCurso'),
-    path(r'deleteCourse/<int:idCourse>', views.CursoView.deleteCourse, name='deleteCourse'),
+    path(r'categoria', login_required(views.CursoView.as_view()), name='categoria'),
+    path(r'allCategorias', login_required(views.CursoView.allCategorias), name='categoriaAll'),
+    path(r'deleteCategoria/<int:idCategoria>', login_required(views.CursoView.deleteCategoria), name='deleteCategoria'),
+    path(r'createEditCategoria', login_required(views.CursoView.createEditCategoria), name='createEditCategoria'),
+    path(r'', login_required(views.CursoView.as_view()), name='cursos'),
+    path(r'crearEditarCurso', login_required(views.CursoView.crearEditarCurso), name='crearEditarCurso'),
+    path(r'deleteCourse/<int:idCourse>', login_required(views.CursoView.deleteCourse), name='deleteCourse'),
 ]
