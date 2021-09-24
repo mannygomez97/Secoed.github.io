8dcaeb9c5ee4cf8d8f9fed61323bab9c0266d353
Criss Intriago <jintriagos_96@outlook.com>
2021-09-24 04:13:37 -0500

    pgdb

--- secoed/settings.py
+++ secoed/settings.py
@@ -109,8 +109,8 @@ WSGI_APPLICATION = 'secoed.wsgi.application'
 CONEXION_NAME = 'db_pre_secoed'
 CONEXION_USER = 'secoed'
 CONEXION_PASSWORD = 'secoed2021'
-CONEXION_HOST = '95.216.216.98'
-CONEXION_PORT = 5434
+CONEXION_HOST = 'pgdb'
+CONEXION_PORT = 5432
 
 #CONEXION PRODUCCION --> debe regitrar su conexion si trabajara con produccion
 #CONEXION_NAME = 'db_pro_secoed'
