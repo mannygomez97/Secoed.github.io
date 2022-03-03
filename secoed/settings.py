2170265d80e42613f4f6c079b49f868b38783952
Criss Intriago <jintriagos_96@outlook.com>
2022-03-03 02:01:47 -0500

    conexion

--- secoed/settings.py
+++ secoed/settings.py
@@ -102,22 +102,21 @@ WSGI_APPLICATION = 'secoed.wsgi.application'
 
 # CONEXION DEVELOPER --> debe regitrar su conexion si trabajara con preproduccion
 
-CONEXION_NAME = 'db_secoed'
+#CONEXION_NAME = 'db_secoed'
+#CONEXION_USER = 'secoed'
+#CONEXION_PASSWORD = 'secoed2021'
+#CONEXION_HOST = '95.216.216.98'
+#CONEXION_HOST = 'localhost'
+#CONEXION_PORT = '5432'
+
+
+#CONEXION PRODUCCION --> debe regitrar su conexion si trabajara con produccion
+CONEXION_NAME = 'db_pro_secoed'
 CONEXION_USER = 'secoed'
 CONEXION_PASSWORD = 'secoed2021'
-CONEXION_HOST = '95.216.216.98'
-#CONEXION_HOST = 'localhost'
+CONEXION_HOST = 'pgdb'
+CONEXION_PORT = 5432
 
-CONEXION_PORT = '5432'
-
-"""""
-# CONEXION PRODUCCION --> debe regitrar su conexion si trabajara con produccion
-# CONEXION_NAME = 'db_pro_secoed'
-# CONEXION_USER = 'secoed'
-# CONEXION_PASSWORD = 'secoed2021'
-# CONEXION_HOST = 'pgdb'
-# CONEXION_PORT = 5432
-"""""
 
 DATABASES = {
     'default': {
