4a71911a0cf6b32e81d1b12a1aea5fc5a1f3909d
Criss Intriago <jintriagos_96@outlook.com>
2022-03-18 16:37:05 -0500

    Cambios

--- secoed/settings.py
+++ secoed/settings.py
@@ -108,8 +108,8 @@ WSGI_APPLICATION = 'secoed.wsgi.application'
 CONEXION_NAME = 'db_secoed'
 CONEXION_USER = 'secoed'
 CONEXION_PASSWORD = 'secoed2021'
-CONEXION_HOST = 'pgdb'
-CONEXION_PORT = '5432'
+CONEXION_HOST = '95.216.216.98'
+CONEXION_PORT = '5434'
 
 
 #CONEXION_NAME = 'db_secoed'
