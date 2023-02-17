6c3531b2c400b72be2a747eed355495af13c4e21
miguelmacias99 <miguelmaciaszambrano123@gmail.com>
2023-02-17 16:03:34 -0500

    Agregar auditorias
    SECOED

--- auditoria/apps.py
+++ auditoria/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.db import connection
 import datetime
+import pandas as pq
 
 class AuditoriaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -10,25 +11,87 @@ class AuditoriaConfig(AppConfig):
 m_CreateAccion = "CREATE"
 m_UpdateAccion = "UPDATE"
 m_DeleteAccion = "DELETE"
+
+m_TipoAdvertencia = "WARNING"
+m_TipoError = "ERROR"
+
+class DatoFormulario:
+    def __init__(self, nombre, valor):
+        self.nombre = nombre
+        self.valor = valor
+
 class GeneradorAuditoria:
     def GenerarAuditoriaCrear(self, nombreTabla, jsonNew, idUsuario):
-        InsertAuditoria(nombreTabla, m_CreateAccion, jsonNew, "", idUsuario)
-    def GenerarAuditoriaActualizar(self, nombreTabla, jsonNew, jsonOld, idUsuario):
-        InsertAuditoria(nombreTabla, m_UpdateAccion, jsonNew, jsonOld, idUsuario)
-    def GenerarAuditoriaBorrar(self, nombreTabla, jsonOld, idUsuario):
-        InsertAuditoria(nombreTabla, m_DeleteAccion, "", jsonOld, idUsuario)
+        idRegistro = GetLastId(nombreTabla)
+        InsertAuditoria(nombreTabla, idRegistro, m_CreateAccion, jsonNew, "", idUsuario)
+    def GenerarAuditoriaActualizar(self, nombreTabla, idRegistro, jsonNew, jsonOld, idUsuario):
+        InsertAuditoria(nombreTabla, idRegistro, m_UpdateAccion, jsonNew, jsonOld, idUsuario)
+    def GenerarAuditoriaBorrar(self, nombreTabla, idRegistro, jsonOld, idUsuario):
+        InsertAuditoria(nombreTabla, idRegistro, m_DeleteAccion, "", jsonOld, idUsuario)
 
-# Métodos para el registro de errores
-class GeneradorErrores:
-    i = 0
+    # Métodos para generar JSON
+    def GenerarJSONExistente(self, nombreTabla, idDictionary):
+        return generateSelectJSON(nombreTabla, idDictionary)
+    def GenerarJSONNuevo(self, nombreTabla):
+        return generateSelectJSON(nombreTabla, {'pk': GetLastId(nombreTabla)})
+
+    # Métodos para el registro de errores
+    def CrearAuditoriaAdvertencia(self, proceso, excepcion, idUsuario):
+        InsertError(proceso, m_TipoAdvertencia, excepcion, idUsuario)
+    def CrearAuditoriaError(self, proceso, excepcion, idUsuario):
+        InsertError(proceso, m_TipoError, excepcion, idUsuario)
 
 # Métodos adicionales
-def InsertAuditoria(tablaName, command, jsonNew, jsonOld, idUsuario):
-    base_sql = "INSERT INTO TB_AUDITORIA (tabla, comando, registroNuevo, registroAnterior, idUsuarioCreacion, fechaCreacion) " \
-       "VALUES (%s, %s, %s, %s, %s, %s);"
+def InsertAuditoria(tablaName, idRegistro, command, jsonNew, jsonOld, idUsuario):
+    base_sql = "INSERT INTO TB_AUDITORIA (tabla, idRegistro, comando, registroNuevo, registroAnterior, idUsuarioCreacion, fechaCreacion) " \
+       "VALUES (%s, %s, %s, %s, %s, %s, %s);"
     fechaHoraActual = datetime.datetime.now()
     valores = [
-        tablaName, command, jsonNew, jsonOld, idUsuario, fechaHoraActual
+        tablaName, idRegistro, command, jsonNew, jsonOld, idUsuario, fechaHoraActual
     ]
     curs = connection.cursor()
     curs.execute(base_sql, valores)
+
+def InsertError(proceso, tipoError, excepcion, idUsuario):
+    base_sql = "INSERT INTO TB_ERRORES (proceso, tipo, excepcion, idUsuario, fechaCreacion) " \
+               "VALUES (%s, %s, %s, %s, %s);"
+    fechaHoraActual = datetime.datetime.now()
+    valores = [
+        proceso, tipoError, excepcion, idUsuario, fechaHoraActual
+    ]
+    curs = connection.cursor()
+    curs.execute(base_sql, valores)
+
+def GetPrimaryKeyColumnName(nombreTabla):
+    base_sql = "SELECT a.attname FROM pg_index i JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey)" \
+                "WHERE  i.indrelid = '" + nombreTabla + "'::regclass AND i.indisprimary;"
+    curs = connection.cursor()
+    curs.execute(base_sql)
+    return curs.fetchone()[0]
+
+def GetLastId(nombreTabla):
+    pkField = GetPrimaryKeyColumnName(nombreTabla)
+    base_sql = "SELECT coalesce(MAX(" + pkField + "), 0) FROM " + nombreTabla
+    curs = connection.cursor()
+    curs.execute(base_sql)
+    return curs.fetchone()[0]
+
+def generateSelectJSON(nombreTabla, idDictionary):
+    primaryKeyFild = GetPrimaryKeyColumnName(nombreTabla)
+    base_sql = "SELECT * FROM " + nombreTabla + " WHERE " + primaryKeyFild + " = " + str(idDictionary["pk"])
+    curs = connection.cursor()
+    curs.execute(base_sql)
+    sqlDatas = curs.fetchone()
+
+    data = []
+    for sqlData in sqlDatas:
+        if type(sqlData).__name__ == 'datetime':
+            date_string = f'{sqlData:%Y-%m-%d %H:%M:%S}'
+            data.append(str(date_string))
+        else:
+            data.append(str(sqlData))
+        a = 1
+
+    cols = list(map(lambda x: x[0], curs.description))
+    lista = pq.DataFrame([data], columns=cols)
+    return lista.to_json(orient ='records')
--- eva/views/categoria/views.py
+++ eva/views/categoria/views.py
@@ -4,8 +4,10 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from eva.models import Categoria, Ciclo, Ciclo2
 from eva.forms import CategoriaForm
 from django.http import JsonResponse
+from auditoria.apps import GeneradorAuditoria
 
-
+m_Proceso = "CATEGORIA"
+m_NombreTabla = "pt_categoria"
 class CategoryListView(ListView):
     model = Categoria
     template_name = 'categoria/list.html'
@@ -21,10 +23,8 @@ class CategoryListView(ListView):
         context['url_list'] = reverse_lazy('eva:list-category')
         return context
 
-
 class CategoryCreateView(CreateView):
     model = Categoria
-    
     form_class = CategoriaForm
     template_name = "categoria/create.html"
     success_url = reverse_lazy('eva:list-category')
@@ -42,16 +42,20 @@ class CategoryCreateView(CreateView):
                     error = 'No han ocurrido errores'
                     response = JsonResponse({'message': message, 'error': error})
                     response.status_code = 201
+                    newJson = GeneradorAuditoria().GenerarJSONNuevo(m_NombreTabla)
+                    GeneradorAuditoria().GenerarAuditoriaCrear(m_NombreTabla, newJson, request.user.id)
                     return response
                 else:
                     message = f'{self.model.__name__} no se pudo registrar!'
                     error = form.errors
                     response = JsonResponse({'message': message, 'error': error})
                     response.status_code = 400
+                    GeneradorAuditoria().CrearAuditoriaAdvertencia(m_Proceso, str(error), request.user.id)
                     return response
         except Exception as e:
             message = 'error'
             response = JsonResponse({'message': message, 'error': str(e)})
+            GeneradorAuditoria().CrearAuditoriaError(m_Proceso, str(e), request.user.id)
         return JsonResponse(response)
 
     def get_context_data(self, **kwargs):
@@ -60,7 +64,6 @@ class CategoryCreateView(CreateView):
         context['action'] = 'add'
         context['list_url'] = reverse_lazy('eva:list-category')
         return context
-    
 
 class CategoryUpdateView(UpdateView):
     model = Categoria
@@ -74,20 +77,25 @@ class CategoryUpdateView(UpdateView):
            if request.is_ajax():
             form = self.form_class(request.POST, instance=self.get_object())
             if form.is_valid():
+                oldJson = GeneradorAuditoria().GenerarJSONExistente(m_NombreTabla, kwargs)
                 form.save()
+                newJson = GeneradorAuditoria().GenerarJSONExistente(m_NombreTabla, kwargs)
                 message = f'{self.model.__name__} actualizado correctamente'
                 error = 'No hay error'
                 response = JsonResponse({'message': message, 'error': error})
                 response.status_code = 201
+                GeneradorAuditoria().GenerarAuditoriaActualizar(m_NombreTabla, kwargs["pk"], newJson, oldJson, request.user.id)
                 return response
             else:
                 message = f'{self.model.__name__} no se pudo actualizar!'
                 error = form.errors
                 response = JsonResponse({'message': message, 'error': error})
                 response.status_code = 400
+                GeneradorAuditoria().CrearAuditoriaAdvertencia(m_Proceso, str(error), request.user.id)
                 return response
         except Exception as e:
             data['error'] = str(e)
+            GeneradorAuditoria().CrearAuditoriaError(m_Proceso, str(e), request.user.id)
         return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
@@ -97,13 +105,13 @@ class CategoryUpdateView(UpdateView):
         context['list_url'] = reverse_lazy('eva:list-category')
         return context
 
-
 class CategoryDeleteView(DeleteView):
     model = Categoria
     success_url = reverse_lazy('eva:list-category')
 
     def delete(self, request, *args, **kwargs):
         if request.is_ajax():
+            oldJson = GeneradorAuditoria().GenerarJSONExistente(m_NombreTabla, kwargs)
             category = self.get_object()
             category.state = False
             category.save()
@@ -111,6 +119,7 @@ class CategoryDeleteView(DeleteView):
             errors = 'No se encontraron errores'
             response = JsonResponse({'message': message, 'error': errors})
             response.status_code = 201
+            GeneradorAuditoria().GenerarAuditoriaBorrar(m_NombreTabla, kwargs["pk"], oldJson, request.user.id)
             return response
         else:
             return redirect('eva:list-category')
