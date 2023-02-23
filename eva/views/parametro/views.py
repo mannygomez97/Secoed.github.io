3348b94d2979fc2f5745bb78efbfd222b27d4a90
miguelmacias99 <miguelmaciaszambrano123@gmail.com>
2023-02-22 20:00:28 -0500

    Agregar auditoría en Parámetros

--- eva/views/parametro/views.py
+++ eva/views/parametro/views.py
@@ -4,8 +4,10 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from eva.models import Parametro, Ciclo
 from eva.forms import ParametroForm
 from django.http import JsonResponse
+from auditoria.apps import GeneradorAuditoria
 
-
+m_Proceso = "PARAMETROS-CABECERA"
+m_NombreTabla = "pt_parametro"
 class ParameterListView(ListView):
     model = Parametro
     template_name = 'parametro/list.html'
@@ -38,15 +40,19 @@ class ParameterCreateView(CreateView):
                     error = 'No han ocurrido errores'
                     response = JsonResponse({'message': message, 'error': error})
                     response.status_code = 201
+                    newJson = GeneradorAuditoria().GenerarJSONNuevo(m_NombreTabla)
+                    GeneradorAuditoria().GenerarAuditoriaCrear(m_NombreTabla, newJson, request.user.id)
                     return response
                 else:
                     message = f'{self.model.__name__} no se pudo registrar!'
                     error = form.errors
                     response = JsonResponse({'message': message, 'error': error})
                     response.status_code = 400
+                    GeneradorAuditoria().CrearAuditoriaAdvertencia(m_Proceso, str(error), request.user.id)
                     return response
         except Exception as e:
             data['error'] = str(e)
+            GeneradorAuditoria().CrearAuditoriaError(m_Proceso, str(e), request.user.id)
         return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
@@ -69,20 +75,25 @@ class ParameterUpdateView(UpdateView):
            if request.is_ajax():
             form = self.form_class(request.POST, instance=self.get_object())
             if form.is_valid():
+                oldJson = GeneradorAuditoria().GenerarJSONExistente(m_NombreTabla, kwargs)
                 form.save()
+                newJson = GeneradorAuditoria().GenerarJSONExistente(m_NombreTabla, kwargs)
                 message = f'{self.model.__name__} actualizado correctamente'
                 error = 'No hay error'
                 response = JsonResponse({'message': message, 'error': error})
                 response.status_code = 201
+                GeneradorAuditoria().GenerarAuditoriaActualizar(m_NombreTabla, kwargs["pk"], newJson, oldJson, request.user.id)
                 return response
             else:
                 message = f'{self.model.__name__} no se pudo actualizar!'
                 error = form.errors
                 response = JsonResponse({'message': message, 'error': error})
                 response.status_code = 400
+                GeneradorAuditoria().CrearAuditoriaAdvertencia(m_Proceso, str(error), request.user.id)
                 return response
         except Exception as e:
             data['error'] = str(e)
+            GeneradorAuditoria().CrearAuditoriaError(m_Proceso, str(e), request.user.id)
         return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
@@ -100,11 +111,13 @@ class ParameterDeleteView(DeleteView):
     def delete(self, request, *args, **kwargs):
         if request.is_ajax():
             parameter = self.get_object()
+            oldJson = GeneradorAuditoria().GenerarJSONExistente(m_NombreTabla, kwargs)
             parameter.delete()
             message = f'{self.model.__name__} eliminado correctamente!'
             errors = 'No se encontraron errores'
             response = JsonResponse({'message': message, 'error': errors})
             response.status_code = 201
+            GeneradorAuditoria().GenerarAuditoriaBorrar(m_NombreTabla, kwargs["pk"], oldJson, request.user.id)
             return response
         else:
             return redirect('eva:list-parameter')
